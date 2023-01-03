20052144d991f5a81e679ec97cf9adcb871cd0ff
Carlos Garavito <cgaravitoc@unal.edu.co>
2023-01-03 16:18:48 -0500

    first commit

---
+++ python/tree.py
@@ -0,0 +1,35 @@
+'''
+This function print the tree folder structure for the current directory
+'''
+
+from pathlib import Path
+import os
+
+PROJECT_PATH = os.getcwd()
+
+# prefix components:
+space =  '    '
+branch = '│   '
+# pointers:
+tee =    '├── '
+last =   '└── '
+
+
+def tree(dir_path: Path, prefix: str=''):
+    """A recursive generator, given a directory Path object
+    will yield a visual tree structure line by line
+    with each line prefixed by the same characters
+    """    
+    contents = list(dir_path.iterdir())
+    # contents each get pointers that are ├── with a final └── :
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        yield prefix + pointer + path.name
+        if path.is_dir(): # extend the prefix and recurse:
+            extension = branch if pointer == tee else space 
+            # i.e. space because last, └── , above so no more |
+            yield from tree(path, prefix=prefix+extension)
+
+for line in tree(Path(PROJECT_PATH)):
+    print(line)
+    
